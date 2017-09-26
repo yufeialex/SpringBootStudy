ffa00e8e1d3f05b84c8348bf36bf1fb69bbdd070
yufeialex <yufeialex@163.com>
2017-09-26 22:42:20 +0800

    init springBoot project

---
+++ springbootstarterhello/src/main/java/com/wisely/spring_boot_starter_hello/HelloService.java
@@ -0,0 +1,18 @@
+package com.wisely.spring_boot_starter_hello;
+
+
+public class HelloService {
+    private String msg;
+
+    public String sayHello() {
+        return "Hello" + msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
