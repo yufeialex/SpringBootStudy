933b4b9e0ffd38a50692ced6e8e3935b310e2581
yufeialex <yufeialex@163.com>
2017-12-15 20:26:05 +0800

    修改过时的注解；去掉无用的导入

--- highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
+++ highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -1,7 +1,7 @@
 package com.wisely.highlight_springmvc4;
 
-import java.util.List;
-
+import com.wisely.highlight_springmvc4.interceptor.DemoInterceptor;
+import com.wisely.highlight_springmvc4.messageconverter.MyMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,17 +9,11 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import com.wisely.highlight_springmvc4.interceptor.DemoInterceptor;
-import com.wisely.highlight_springmvc4.messageconverter.MyMessageConverter;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc// 1
@@ -39,8 +33,8 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {// 2
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/assets/**").addResourceLocations(
-                "classpath:/assets/");// 3
+        registry.addResourceHandler("/assets/**") // 这是设置对外暴露的访问路径
+                .addResourceLocations("classpath:/assets/"); // 这是指文件放置的目录
 
     }
 
